885d43c80c15ed5d5456dfa9bbb5e8fd83906466
samkelisojam <65507568+samkelisojam@users.noreply.github.com>
2025-01-16 21:45:18 +0200

    changes working one
    commit

--- reactapp2.client/src/App.jsx
+++ reactapp2.client/src/App.jsx
@@ -8,11 +8,11 @@ function App() {
     
 
     return (
-        <>
+    
             <h1 >Hello React</h1>
-            <p>React App 2025.</p>
           
-        </>
+          
+      
     );
     
     
--- reactapp2.client/src/main.jsx
+++ reactapp2.client/src/main.jsx
@@ -7,10 +7,10 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <chskraProvider>
+      
             <App />
 
-        </chskraProvider>
+       
 
   </React.StrictMode>,
 )
