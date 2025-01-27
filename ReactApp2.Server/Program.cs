aa89e87479a95ef90de417230a00bedf88a25862
samkelisojam <65507568+samkelisojam@users.noreply.github.com>
2025-01-27 10:36:47 +0200

    Coonected back end and front end API

--- ReactApp2.Server/Program.cs
+++ ReactApp2.Server/Program.cs
@@ -1,21 +1,30 @@
 using ReactApp2.Server.Data;
-using System;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(opts =>
-opts.UseSqlServer(connString, opts =>
-{
-   
-}));
+    opts.UseSqlServer(connString));
+
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder
+            .WithOrigins("https://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "ReactApp2 API", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -26,16 +35,23 @@ app.UseStaticFiles();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "ReactApp2 API v1");
+    });
 }
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseAuthorization();
+app.UseCors("AllowSpecificOrigin");
 
 app.MapControllers();
 
 app.MapFallbackToFile("/index.html");
- SeedData.EnsureEntityPopulated(app);
+
+SeedData.EnsureEntityPopulated(app);
 
 app.Run();
--- ReactApp2.Server/obj/Debug/net8.0/ReactApp2.Server.AssemblyInfo.cs
+++ ReactApp2.Server/obj/Debug/net8.0/ReactApp2.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactApp2.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21869b2ec4c52fff205da725dca349a3f0991f4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62dc4c2565f8aa12c344196bc2a348cd059133e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactApp2.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactApp2.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
